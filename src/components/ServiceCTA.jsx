d94c1d37a38e241bad989fccac8606134b04f44e
jeel popat <jeelpopat999@gmail.com>
2026-06-24 16:33:05 +0530

    first commit

--- src/components/ServiceCTA.jsx
+++ src/components/ServiceCTA.jsx
@@ -32,7 +32,7 @@ const ServiceCTA = ({
     setStatus('Sending request...');
 
     try {
-      const response = await fetch('http://localhost:5000/api/service', {
+      const response = await fetch('https://digital-marketing-services-backend.onrender.com/api/service', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
